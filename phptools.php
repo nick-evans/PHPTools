d33b1eb2ee8096d456e6c2add11d36fc84a0ed17
nickvps <ne20142014@gmail.com>
2017-01-05 19:53:25 +0100

    1483642405

--- phptools.php
+++ phptools.php
@@ -2,5 +2,14 @@
 function say($str) {
     echo $str;
 }
-
-?>
+function filecontents($file) {
+    return file_get_contents($file);
+}
+function error($str) {
+    trigger_error($str);
+}
+function randomnumber() {
+   $characters = '0123456789';
+   $charactersLength = strlen($characters);
+   return $characters[rand(0, $charactersLength - 1)];
+}
